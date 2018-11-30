89cd9b32d2f8bfc044c4eb0564f2b9eb7c59c5ef
Anh Vuong <anhvvuong0212@gmail.com>
2018-11-30 14:50:36 +1100

    Create login form Fetch users' data from express app Perform validation on client-side

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: [],
+    username: '',
+    password: '',
+    isValidate: null,
+  };
+
+  fetchUsersDataFromBackend = () =>
+    fetch('/users')
+      .then(res => res.json())
+      .then(users => this.setState({ users }));
+
+  onValueChange = (key, event) =>
+    this.setState({ [key]: event.target.value });
+
+  onLoginSubmit = (event, username, password) => {
+
+    this.fetchUsersDataFromBackend();
+
+    let users = this.state.users;
+
+    users = users.filter(user =>
+      user.username === username && user.password === password
+    );
+
+    this.setState({
+      isValidate: true && users.length !== 0,
+      users: []
+    });
+
+    event.preventDefault();
+  }
+
   render() {
+
+    const {
+      users,
+      username,
+      password,
+      isValidate
+    } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>This is a log in portal</h1>
         </header>
+        <form onSubmit={(event) => this.onLoginSubmit(event, username, password)}>
+          <div className="login-window">
+            <div className="login-section">
+              <h5>Username:</h5>
+              <input
+                type="text"
+                id="login-username"
+                onChange={(event) => this.onValueChange('username', event)}
+                value={username}
+              >
+              </input>
+            </div>
+            <div className="login-section">
+              <h5>Password:</h5>
+              <input
+                type="password"
+                id="login-password"
+                onChange={(event) => this.onValueChange('password', event)}
+                value={password}
+              >
+              </input>
+            </div>
+            {!(isValidate || isValidate === null) &&
+              <h5>Wrong password, please try again!</h5>
+            }
+            <button id="login-button">Login</button>
+          </div>
+          <div>
+            <h5>Your username: {username}</h5>
+          </div>
+          <div>
+            <h5>Your password: {password}</h5>
+          </div>
+        </form>
       </div>
     );
   }
